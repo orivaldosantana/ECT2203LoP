95c09bd6cb5fd0a54270220d5dd6d6c3915e37d5
orivaldosantana <orivaldo@gmail.com>
2017-05-30 10:26:27 -0300

    Update ect.js

--- lab0/codigo_base/ect.js
+++ lab0/codigo_base/ect.js
@@ -20,3 +20,10 @@ function divisaoInteira(x,y) {
 	return Math.floor(x/y);
 }
 
+// Retorna um  número inteiro entre min (incluso) e max (incluso)
+// Usando Math.round() vai lhe dar uma distribuição não uniforme!
+// Referência: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random 
+function aleatorio(min,max) {
+	return Math.floor(Math.random() * (1+max - min)) + min;	
+}
+
